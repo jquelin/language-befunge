74ece704fd30248c5ccf131ee500f276883a34e3
Jérôme Quelin <jquelin@gmail.com>
2008-11-11 16:05:10 +0100

    new extnesion ROMA

---
+++ lib/Language/Befunge/lib/ROMA.pm
@@ -0,0 +1,79 @@
+#
+# This file is part of Language::Befunge.
+# Copyright (c) 2001-2008 Jerome Quelin, all rights reserved.
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the same terms as Perl itself.
+#
+#
+
+package Language::Befunge::lib::ROMA;
+
+use strict;
+use warnings;
+
+use Language::Befunge::Vector;
+
+sub new { return bless {}, shift; }
+
+sub C { $_[1]->get_curip->spush(100); }
+sub D { $_[1]->get_curip->spush(500); }
+sub I { $_[1]->get_curip->spush(1); }
+sub L { $_[1]->get_curip->spush(50); }
+sub M { $_[1]->get_curip->spush(1000); }
+sub V { $_[1]->get_curip->spush(5); }
+sub X { $_[1]->get_curip->spush(10); }
+
+
+1;
+
+__END__
+
+
+=head1 NAME
+
+Language::Befunge::IP::lib::ROMA - Roman numerals library
+
+
+
+=head1 DESCRIPTION
+
+The ROMA fingerprint (0x524f4d41) allows to get standard values of roman
+numbers. Note that the new instructions will just push digits, you still
+have to do the arithmetic yourself. Executing MCMLXXXIV will not leave 1984
+on the stack. But executing C<MCM\-+LXXX+++IV\-++> should.
+
+
+=head1 FUNCTIONS
+
+=head2 new
+
+Create a new ROMA instance.
+
+
+=head2 C D I L M V X
+
+Push the corresponding roman value (M=1000, D=500, etc.) onto the stack.
+
+
+
+=head1 SEE ALSO
+
+L<Language::Befunge>, L<http://catseye.tc/projects/funge98/library/ROMA.html>.
+
+
+
+=head1 AUTHOR
+
+Jerome Quelin, C<< <jquelin@cpan.org> >>
+
+
+=head1 COPYRIGHT & LICENSE
+
+Copyright (c) 2001-2008 Jerome Quelin, all rights reserved.
+
+This program is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+
+=cut
