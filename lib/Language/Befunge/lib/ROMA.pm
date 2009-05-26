da7c6413a230b3354f5ef38cbb4ed76721ca7d61
Jérôme Quelin <jquelin@gmail.com>
2009-05-26 17:17:44 +0200

    updating copyright

--- lib/Language/Befunge/lib/ROMA.pm
+++ lib/Language/Befunge/lib/ROMA.pm
@@ -1,6 +1,6 @@
 #
 # This file is part of Language::Befunge.
-# Copyright (c) 2001-2008 Jerome Quelin, all rights reserved.
+# Copyright (c) 2001-2009 Jerome Quelin, all rights reserved.
 #
 # This program is free software; you can redistribute it and/or modify
 # it under the same terms as Perl itself.
@@ -90,7 +90,7 @@ Jerome Quelin, C<< <jquelin@cpan.org> >>
 
 =head1 COPYRIGHT & LICENSE
 
-Copyright (c) 2001-2008 Jerome Quelin, all rights reserved.
+Copyright (c) 2001-2009 Jerome Quelin, all rights reserved.
 
 This program is free software; you can redistribute it and/or modify
 it under the same terms as Perl itself.
